fa1724a3755bf706f01395b9d646810333149499
paul-saac <johnpaul.saac.legaspi@gmail.com>
2026-03-04 19:08:38 +0800

    Implementing Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
+import Contact from "./pages/Contact.jsx"
 import './App.css'
 
 function App() {
-  
     return (
         <div>
-            
+            <nav style={{ display: "flex", gap: "20px" }}>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
         </div>
     )
 }
 
-export default App
+export default App;
